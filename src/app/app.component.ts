b3d0fc516ab4ffd294fe525e26ac350a00eed60b
Tina <xialiu79@yahoo.ca>
2023-07-26 15:19:20 -0400

    Customize component router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
